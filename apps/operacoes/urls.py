860601551ad28bdcde4aa75357d98b412a13d468
JOSE RIBEIRO <jribeiro@ufu.br>
2026-02-15 15:23:23 -0300

    refatoracao: ui_atribuicoes estabilizado

--- apps/operacoes/urls.py
+++ apps/operacoes/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("assistidos/", include("apps.operacoes.ui_assistidos.urls")),
     path("beneficios/", include("apps.operacoes.ui_beneficios.urls")),
-    path("atribuicoes/", include("apps.operacoes.atribuicoes.urls")),
+    path("atribuicoes/", include("apps.operacoes.ui_atribuicoes.urls")),
     path("entregas/", include("apps.operacoes.entregas.urls")), 
     path("consultas/", include("apps.operacoes.consultas.urls")),
     path("", views.home_view, name="operacoes_home"),
