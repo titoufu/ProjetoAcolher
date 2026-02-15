fc42160281b48c2f1fa1d13cde9fccf53c08dfb3
JOSE RIBEIRO <jribeiro@ufu.br>
2026-02-15 15:26:25 -0300

    refatoracao: ui_entregas estabilizado

--- apps/operacoes/urls.py
+++ apps/operacoes/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("assistidos/", include("apps.operacoes.ui_assistidos.urls")),
     path("beneficios/", include("apps.operacoes.ui_beneficios.urls")),
     path("atribuicoes/", include("apps.operacoes.ui_atribuicoes.urls")),
-    path("entregas/", include("apps.operacoes.entregas.urls")), 
+    path("entregas/", include("apps.operacoes.ui_entregas.urls")), 
     path("consultas/", include("apps.operacoes.consultas.urls")),
     path("", views.home_view, name="operacoes_home"),
 ]
