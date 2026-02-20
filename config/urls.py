d4871065077baf6ddf9ed52ce9cafffe31cf5deb
JOSE RIBEIRO <jribeiro@ufu.br>
2026-02-20 11:17:05 -0300

    Ajuste na urls.py  => rota home

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect("/accounts/login/")  # ou "/operacoes/"
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("operacoes/", include("apps.operacoes.urls")),
     path("impressos/", include("apps.impressos.urls")),
-
 ]
